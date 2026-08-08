3f4d46b624a803b456b02a49a964e2a5ed90ac99
ShahdShamali <shamali.shahed2007@gmail.com>
2026-08-08 15:31:54 -0700

    apple.com & flex project & position

---
+++ main.js
@@ -0,0 +1,27 @@
+for(let i=1; i<21; i++){
+    if (i %2 != 0){
+      console.log(i)          //print the odd num
+    }
+}
+for(let i=1; i<21; i+=2){
+    console.log(i)               // print the odd num
+}
+for(let i=100 ; i>-1 ; i--){
+    if (i%3 == 0){
+        console.log(i)
+    }
+}
+for(let i=4; i>=-3.5 ; i=-1.5){
+    console.log(i)
+}
+let(sum=0)
+for(let i=1; i<101 ; i++){
+    sum+=i
+}
+console.log("The sum is",sum)
+
+let(factorial=0);
+for(let i=1 ; i<13 ;i++){
+    factorial+=i;
+}
+console.log(factorial)
